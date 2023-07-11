4104af4a22d96c41c0bb28738e009d66e2888d19
Josef Veselý <49988965+JosefVesely@users.noreply.github.com>
2023-07-11 23:06:42 +0200

    Command line args are now parsed using getopt.h

--- image_to_ascii.c
+++ image_to_ascii.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <getopt.h>
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
@@ -8,47 +9,73 @@
 #include "stb_image/stb_image_write.h"
 #include "stb_image/stb_image_resize.h"
 
-char* symbols = "@#$?!~:-.^` ";
+char* characters = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+
+void print_usage(void)
+{
+    printf("Usage: image_to_ascii.exe --input image.png [--output ascii.txt] [--width 50] [--chars \"@#?|:. \"]\n");
+    printf("  --help: shows this message\n");
+    printf("  --input {image.png}: input file path\n");
+    printf("  --output {ascii.txt}: output file path, \"output.txt\" if none (optional)\n");
+    printf("  --width {50}: width of output (optional)\n");
+    printf("  --chars {\"@#?|:. \"}: characters to be used (optional)\n");
+}
 
 
 int main(int argc, char** argv)
 {
-    // Parse arguments from the command line
+    // Exit if no command line arguments are given
 
-    char* input_filepath;
-    char* output_filepath;
-    int desired_width;
-    bool resize_image = true;
-
-    if (argc == 1) {  // Usage
-        printf("Usage: image_to_ascii.exe input [output] [width]\n");
-        printf("   input: file path of the image to convert\n");
-        printf("   output: file path of the output file (optional)\n");
-        printf("   width: width of output (optional)\n");
-        printf("   chars: characters to be used (optional)\n");
+    if (argc == 1) {
+        print_usage();
         return EXIT_FAILURE;
     }
 
-    if (argc >= 2) {  // input
-        input_filepath = argv[1];
-    }
+    // Parse arguments from the command line
 
-    if (argc >= 3) {  // output
-        output_filepath = argv[2];
-    }
-    else {
-        output_filepath = "output.txt";
-    }
+    char* input_filepath;
+    char* output_filepath = "output.txt";
+    int desired_width;
+    bool resize_image = false;
 
-    if (argc >= 4) {  // width
-        desired_width = atoi(argv[3]);
-    }
-    else {
-        resize_image = false;
-    }
+    struct option long_options[] =
+    {
+        {"help", no_argument, NULL, 'h'},
+        {"input", required_argument, NULL, 'i'},
+        {"output", optional_argument, NULL, 'o'},
+        {"width", optional_argument, NULL, 'w'},
+        {"chars", optional_argument, NULL, 'c'},
+        {0, 0, 0, 0}
+    };
 
-    if (argc >= 5) {  // chars
-        symbols = argv[4];
+    int c, option_index;
+    char *short_options = "hi:o::w::c::";
+
+    while ((c = getopt_long(argc, argv, short_options, long_options, NULL)) != EOF)
+    {
+        switch (c)
+        {
+        case 'h':
+            print_usage();
+            return EXIT_FAILURE;
+
+        case 'i':
+            input_filepath = optarg;
+            break;
+
+        case 'o':
+            output_filepath = optarg;
+            break;
+
+        case 'w':
+            desired_width = atoi(optarg);
+            resize_image = true;
+            break;
+
+        case 'c':
+            characters = optarg;
+            break;
+        }
     }
 
     // Load the image in grayscale
@@ -94,14 +121,23 @@ int main(int argc, char** argv)
        return EXIT_FAILURE;
     }
 
-    fprintf(file_pointer, "Input: %s  Output: %s  Resolution: [%dx%d]\n", input_filepath, output_filepath, desired_width, desired_height);
+    int characters_count = strlen(characters);
+
+    fprintf(
+        file_pointer, 
+        "Input: %s\n"
+        "Output: %s\n"
+        "Resolution: %dx%d\n"
+        "Characters (%d): \"%s\"\n\n",
+        input_filepath, output_filepath, desired_width, desired_height, characters_count, characters
+    );
 
     for (int i = 0; i < desired_height*desired_width; i++) {
         int intensity = image[i];
 
-        int character_index = intensity / (255 / (strlen(symbols)-1));
+        int character_index = intensity / (255 / (float)(characters_count - 1));
 
-        fprintf(file_pointer, "%c", symbols[character_index]);
+        fprintf(file_pointer, "%c", characters[character_index]);
 
         if ((i+1) % desired_width == 0) {
             fprintf(file_pointer, "\n");
