62d991a51ff6018ab441c8cc815c98bc3440b52e
Josef Veselý <49988965+JosefVesely@users.noreply.github.com>
2023-08-21 15:01:23 +0200

    CLI options now work properly + small fixes

--- image_to_ascii.c
+++ image_to_ascii.c
@@ -21,23 +21,24 @@ void reverse_string(char* string) {
     }
 }
 
-void print_usage(void)
+void show_usage(void)
 {
     printf(
-        "\nUsage: img2ascii --input=<FILE> [options] \n\n"
+        "\nUsage: img2ascii -i <FILE> [options] \n\n"
 
         "A command-line tool for converting images to ASCII art \n\n"
 
         "Options: \n"
-        "   -i, --input=<FILE>      Path of the input image file (required) \n"
-        "   -o, --output=<FILE>     Path of the output file (default is \"output.txt\") \n"
-        "   -w, --width=<NUMBER>    Width of the output \n"
-        "   -c, --chars=<STRING>    Characters to be used for the ASCII image \n"
+        "   -i, --input  <FILE>     Path of the input image file (required) \n"
+        "   -o, --output <FILE>     Path of the output file (default is 'output.txt') \n"
+        "   -w, --width  <NUMBER>   Width of the output \n"
+        "   -c, --chars  <STRING>   Characters to be used for the ASCII image \n"
         "   -p, --print             Print the output to the console \n"
         "   -r, --reverse           Reverse the string of characters \n\n"
     
         "GitHub Repository: https://github.com/JosefVesely/Image-to-ASCII \n\n"
     );
+    exit(EXIT_FAILURE);
 }
 
 
@@ -46,41 +47,39 @@ int main(int argc, char** argv)
     // Exit if no command line arguments are given
 
     if (argc == 1) {
-        print_usage();
-        return EXIT_FAILURE;
+        show_usage();
     }
 
     // Parse arguments from the command line
 
-    char* input_filepath;
+    char* input_filepath = NULL;
     char* output_filepath = "output.txt";
-    int reverse_flag = 0;
-    int print_flag = 0;
-    int resize_image = false;
-    int desired_width;
+    bool reverse_flag = false;
+    bool print_flag = false;
+    bool resize_image = false;
+    int desired_width = 0;
 
     struct option long_options[] =
     {
         { "help",    no_argument,       NULL, 'h' },
         { "input",   required_argument, NULL, 'i' },
-        { "output",  optional_argument, NULL, 'o' },
-        { "width",   optional_argument, NULL, 'w' },
-        { "chars",   optional_argument, NULL, 'c' },
+        { "output",  required_argument, NULL, 'o' },
+        { "width",   required_argument, NULL, 'w' },
+        { "chars",   required_argument, NULL, 'c' },
         { "print",   no_argument,       NULL, 'p' },
         { "reverse", no_argument,       NULL, 'r' },
         { 0, 0, 0, 0 }
     };
 
-    int c;
-    const char* short_options = "hi:o::w::c::pr";
+    int option;
+    const char* short_options = "hi:o:w:c:pr";
 
-    while ((c = getopt_long(argc, argv, short_options, long_options, NULL)) != EOF)
+    while ((option = getopt_long(argc, argv, short_options, long_options, NULL)) != EOF)
     {
-        switch (c)
+        switch (option)
         {
         case 'h':
-            print_usage();
-            return EXIT_FAILURE;
+            show_usage();
 
         case 'i':
             input_filepath = optarg;
@@ -108,7 +107,14 @@ int main(int argc, char** argv)
         case 'r':
             reverse_flag = true;
             break;
-        }
+        
+        case '?':
+            exit(EXIT_FAILURE);
+        }     
+    }
+
+    if (input_filepath == NULL) {
+        show_usage();
     }
 
     // Load the image in grayscale
@@ -120,14 +126,20 @@ int main(int argc, char** argv)
 
     if (image == NULL) {
         fprintf(stderr, "Could not load image \n");
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
 
     // Check if the width is a valid value
 
-    if (resize_image && !(desired_width > 0 && desired_width <= width)) {
-        fprintf(stderr, "Argument \"width\" must be an integer between 1 and %d (the original image width) \n", width);
-        return EXIT_FAILURE;
+    if (resize_image) {
+        if (desired_width <= 0) {
+            fprintf(stderr, "Argument 'width' must be greater than 0 \n");
+            exit(EXIT_FAILURE);
+        } 
+        else if (desired_width >= width) {
+            fprintf(stderr, "Argument 'width' can not be greater than the original image width (%dpx) \n", width);
+            exit(EXIT_FAILURE);
+        }
     }
     else if (desired_width == width) {
         resize_image = false;
@@ -153,24 +165,35 @@ int main(int argc, char** argv)
     file_pointer = fopen(output_filepath, "w");
 
     if (file_pointer == NULL) {
-       fprintf(stderr, "Could not create an output file \n");
-       return EXIT_FAILURE;
+        fprintf(stderr, "Could not create an output file \n");
+        exit(EXIT_FAILURE);
     }
 
     if (reverse_flag) {
         reverse_string(characters);
     }
 
-    int characters_count = strlen(characters);
+    // Write some useful information to the file
 
+    fprintf(file_pointer, "img2ascii ");
+
+    for (int i = 1; i < argc; i++) {
+        fprintf(file_pointer, "%s ", argv[i]);
+    }
+
+    int characters_count = strlen(characters);
+    
     fprintf(
-        file_pointer, 
+        file_pointer,
+        "\n\n"
         "Input: %s \n"
         "Output: %s \n"
         "Resolution: %dx%d \n"
-        "Characters (%d): \"%s\"\n \n",
+        "Characters (%d): \"%s\" \n\n",
         input_filepath, output_filepath, desired_width, desired_height, characters_count, characters
     );
+
+    // Write the ASCII art to the file
 
     for (int i = 0; i < desired_height * desired_width; i++) {
         int intensity = image[i];
@@ -190,7 +213,7 @@ int main(int argc, char** argv)
         }
     }
 
-    printf("File saved as: %s \n", output_filepath);
+    printf("\nFile saved as '%s' \n", output_filepath);
 
     fclose(file_pointer);
     stbi_image_free(image);
